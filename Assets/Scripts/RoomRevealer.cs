c8078b642efa34e85d33791c05d0ed1a1b9e7f78
Jeremy Kenyon <lanthos@gmail.com>
2019-05-05 14:59:25 -1000

    FogOfWar box added and ability to see door frames inside it

---
+++ Assets/Scripts/RoomRevealer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomRevealer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        if (other.tag == "Player")
+        {
+            Debug.Log($"{other.name} is tagged as player");
+            gameObject.SetActive(false);
+        }
+    }
+}
