27d2e8c0e3f424551bbe7f6499116da7350b5e9d
brnielsen <47045697+brnielsen@users.noreply.github.com>
2019-07-07 20:49:27 -0400

    Added basic Stats/Skills logic. Made a "PlayerTest" prefab.  Added CharacterTest scene.

---
+++ Assets/Scripts/Characters/CharacterStats.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth { get; private set; }
+
+    public Stat damage;
+    public Stat armor;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+      /*  if (Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10);//Testing purposes only
+        }*/
+    }
+
+    public void TakeDamage(int damage)
+    {
+        damage -= armor.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+        currentHealth -= damage;
+        Debug.Log(transform.name + " takes" + damage + " damage.");
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        //meant to be overwritten
+        Debug.Log(transform.name + " died.");
+    }
+}
---
+++ Assets/Scripts/Characters/PlayerStats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+
+    public Stat hacking;
+    public Stat shooting;
+
+
+    //TODO: Add Functionality for Equipment to modify player stats
+}
