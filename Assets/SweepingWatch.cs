7ea344cf109fdd2b00d98b2a33e3cd1ec5ae07a7
Anthony J <lordrahvin@gmail.com>
2019-07-09 00:46:24 -0700

    At night, the statues come to life!
    Added in some general utility files, including a red transparent frame for UI and a rotation script for future security cameras and laser detectors.  On a lark, I put it on the scultptures just to test it out.

---
+++ Assets/SweepingWatch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SweepingWatch : MonoBehaviour
+{
+	public float maxArc = 90f;
+	float speed;
+	float rotationAmount = 0f;
+	Vector3 defaultAngle;
+	Vector3 sweepingAngle;
+
+	private void Start()
+	{
+		speed = Random.Range(0.2f, 0.5f);
+		defaultAngle = transform.forward;
+		sweepingAngle = transform.forward;
+	}
+
+	void Update()
+    {
+		rotationAmount = Mathf.Sin(Time.time * speed) * maxArc;
+		sweepingAngle = Quaternion.AngleAxis(rotationAmount, transform.up) * defaultAngle;
+		transform.localRotation = Quaternion.LookRotation(sweepingAngle);
+	}
+}
