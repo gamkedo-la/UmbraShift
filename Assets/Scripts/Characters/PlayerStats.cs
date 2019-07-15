82b846bd3fbc213efcf4815fe814caea9fa5b26d
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2019-07-15 12:51:12 -0400

    Character Stats/Skills.  Added stats to PlayerStats.

--- Assets/Scripts/Characters/PlayerStats.cs
+++ Assets/Scripts/Characters/PlayerStats.cs
@@ -7,6 +7,8 @@ public class PlayerStats : CharacterStats
 
     public Stat hacking;
     public Stat shooting;
+    public Stat investigation;
+    public Stat stealth;
 
 
     //TODO: Add Functionality for Equipment to modify player stats
