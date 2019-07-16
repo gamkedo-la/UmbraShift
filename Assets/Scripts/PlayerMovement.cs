5923f925de65b360c16e8c31cc40eb0403685fa4
Anthony J <lordrahvin@gmail.com>
2019-07-15 23:24:03 -0700

    Added PlayerMovement script and Tracer object for eventual movment confirmation and path reveal.  Not finished, not hooked up.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+	NavMeshAgent navMeshAgent;
+	MovementTracer tracer;
+
+	private void Start()
+	{
+		
+	}
+}
