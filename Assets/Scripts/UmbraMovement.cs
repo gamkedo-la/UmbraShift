da89714e5cf42dc2bb744f1c5daf296d210fcec2
FlashFire82 <jehopper97@gmailcom>
2019-07-17 11:01:13 -0500

    Cleaned/Fixed UmbraShift Script

--- Assets/Scripts/UmbraMovement.cs
+++ Assets/Scripts/UmbraMovement.cs
@@ -8,17 +8,18 @@ public class UmbraMovement : MonoBehaviour
 {
     NavMeshAgent agent;
     PlayerController pc;
-    Vector3 realScale;
-    Vector3 realColor;
+    
 
     
-    private bool moving;
-    private Vector3 velocity = Vector3.zero;
-    private Vector3 colorVelocity = Vector3.zero;
-    private Vector3 color = Vector3.zero;
-    private Vector4 tempColor = Vector4.zero;
+    public bool isMoving = false;
+    Vector3 velocity = Vector3.zero;
+    Vector3 colorVelocity = Vector3.zero;
+    Vector3 color = Vector3.zero;
+    
+    Vector3 realColor;
+    Vector3 realScale;
 
-    private Vector3 shadowScale;
+    Vector3 shadowScale;
     Renderer rend;
 
     public float shiftTime = 0.25F;
@@ -27,45 +28,47 @@ public class UmbraMovement : MonoBehaviour
     {
         agent = this.GetComponent<NavMeshAgent>();
         pc = this.GetComponent<PlayerController>();
+
         realScale = transform.localScale;
-        moving = false;
         shadowScale = new Vector3(transform.localScale.x, 0.01f, transform.localScale.z);
+
         rend = GetComponent<Renderer>();
-        realColor = new Vector3(rend.material.GetColor("_BaseColor").r, rend.material.GetColor("_BaseColor").g, rend.material.GetColor("_BaseColor").b);
+        realColor = ToVector3(rend.material.GetColor("_BaseColor"));
     }
 
 
     void Update()
     {
         // If player is currentPlayerController, and player is not at destination
-        if (pc == TurnManager.instance.ActivePlayerController && (transform.position.x != agent.destination.x || transform.position.z != agent.destination.z) && !moving)
+        if (pc == TurnManager.instance.ActivePlayerController && agent.remainingDistance > agent.stoppingDistance && !isMoving)
         {
             StartCoroutine(UmbraShift(shiftTime));
-            moving = true;
+            isMoving = true;
 			pc.agent.isStopped = true;
-		}
-            
+        }
         
     }
 
     // Rescale object to 0.01 on y, change color to black
     private IEnumerator UmbraShift(float t)
     {
-        while(transform.localScale.y > 0.011f)
+        while(!Mathf.Approximately(transform.localScale.y, 0.01f)) // Because SmoothDamp only gets arbitrarily close to the target value
         {
 			if (transform.localScale.y <= 0.1f)	{ pc.agent.isStopped = false; }
 			else { pc.agent.isStopped = true; }
+
 			transform.localScale = Vector3.SmoothDamp(transform.localScale, shadowScale, ref velocity, shiftTime);
-            tempColor = rend.material.GetColor("_BaseColor");
-            color = Vector3.SmoothDamp(new Vector3(tempColor.x, tempColor.y, tempColor.z), new Vector3(0, 0, 0), ref colorVelocity, shiftTime);
+            
+            color = Vector3.SmoothDamp(ToVector3(rend.material.GetColor("_BaseColor")), Vector3.zero, ref colorVelocity, shiftTime);
             rend.material.SetColor("_BaseColor", new Color(color.x, color.y, color.z));
+
             yield return new WaitForEndOfFrame();
         }
 
-        
-        // Waits until object is at destination
-        while (transform.position.x != agent.destination.x || transform.position.z != agent.destination.z)
-            yield return new WaitForEndOfFrame();
+
+        // Wait until object is at destination
+        while(agent.velocity != Vector3.zero) { yield return new WaitForEndOfFrame(); }
+
         StartCoroutine(ReSize());
         yield return null;
     }
@@ -73,16 +76,25 @@ public class UmbraMovement : MonoBehaviour
     // After motion stops, rescale object and return color to normal
     private IEnumerator ReSize()
     {
-
-        while(transform.localScale.y < realScale.y - 0.001)
+        // Setting moving here allows for the player to move again before completing the animation back up to normal size
+        isMoving = false;
+        while(!Mathf.Approximately(transform.localScale.y, realScale.y) && isMoving == false)
         {
+            pc.agent.isStopped = true;
+
             transform.localScale = Vector3.SmoothDamp(transform.localScale, realScale, ref velocity, shiftTime);
-            tempColor = rend.material.GetColor("_BaseColor");
-            color = Vector3.SmoothDamp(new Vector3(tempColor.x, tempColor.y, tempColor.z), realColor, ref colorVelocity, shiftTime);
+            
+            color = Vector3.SmoothDamp(ToVector3(rend.material.GetColor("_BaseColor")), realColor, ref colorVelocity, shiftTime);
             rend.material.SetColor("_BaseColor", new Color(color.x, color.y, color.z));
+
             yield return new WaitForEndOfFrame();
-//            print(velocity);
         }
-        moving = false;
+        pc.agent.isStopped = false;
+    }
+
+    // For more convenient color handling
+    private Vector3 ToVector3(Vector4 v4)
+    {
+        return new Vector3(v4.x, v4.y, v4.z);
     }
 }
