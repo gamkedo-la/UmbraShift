8f5a746c8d6c0285db1005570e63c656e754bb4e
Anthony J <lordrahvin@gmail.com>
2019-07-18 23:40:27 -0700

    Finished dynamic mouse cursors.  Cursor changes color when hovering over walkable areas, hackable objects, or enemies.

--- Assets/CursorController.cs
+++ Assets/CursorController.cs
@@ -11,15 +11,15 @@ public class CursorController : MonoBehaviour
 	[SerializeField] private Texture2D attackCursor;
 	[SerializeField] private Texture2D hackCursor;
 	Vector2 topLeft = Vector2.left + Vector2.up;
-	private int walkableAreaLayer = 16384;
-	private int visibleEnemyLayer = 256;
-	private int interactableLayer = 8192;
-	private int combinedLayer = 24832;
+	LayerMask combinedLayer;
+
+	
 
 	// Start is called before the first frame update
 	void Start()
     {
 		Cursor.SetCursor(defaultCursor, topLeft, CursorMode.Auto);
+		combinedLayer = LayerMask.GetMask("WalkableArea","VisibleNPC","InteractableObject");
     }
 
     void Update()
@@ -29,40 +29,34 @@ public class CursorController : MonoBehaviour
 
 	public Texture2D GetLayerUnderMouse()
 	{
-		Texture2D mouseCursor = defaultCursor;
 		PlayerController chr = TurnManager.instance.ActivePlayerController;
-		RaycastHit mouseRayHit;
 		Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-		bool mouseRayCast = Physics.Raycast(mouseRay, out mouseRayHit, 100f, combinedLayer);
-		Debug.Log("Raycast Hit? : " + mouseRayCast);
-		if (mouseRayCast)
-		{
-			Debug.Log("Layer Hit: " + mouseRayHit.collider.gameObject.layer);
-		}
+		bool mouseRayCast = Physics.Raycast(mouseRay, out RaycastHit mouseRayHit, 100f, combinedLayer);
+
 		if (mouseRayCast==false || TurnManager.instance.playersTurn == false || chr == null)
 		{ return defaultCursor; }
 
-		else if (mouseRayHit.collider.gameObject.layer == walkableAreaLayer)
+		else if (mouseRayHit.collider.gameObject.layer == 14)
 		{
 			bool valid = true;
-			float dist = Vector3.Distance(mouseRayHit.point, chr.transform.position);
-			// perhaps check the distance of the AI path instead of direct line-of-sight distance
-			// perhaps do a raycast to check for intervening obstacles?
-			if (dist > chr.maxMoveWithAvailableAP) { valid = false; }
-			if (valid == true) { return moveCursor; } else { return defaultCursor; }
+			//line of sight?  Adjacency?  Max visibility?  Available power?
+			if (valid == true) { return hackCursor; } else { return defaultCursor; }
 		}
-		else if (mouseRayHit.collider.gameObject.layer == visibleEnemyLayer)
+		else if (mouseRayHit.collider.gameObject.layer == 9)
 		{
 			bool valid = true;
 			//check for weapon range?
 			//check for direct line of sight?
 			if (valid == true) { return attackCursor; } else { return defaultCursor; }
 		}
-		else if (mouseRayHit.collider.gameObject.layer == interactableLayer)
+		else if (mouseRayHit.collider.gameObject.layer == 15)
 		{
 			bool valid = true;
-			//line of sight?  Adjacency?  Max visibility?  Available power?
-			if (valid == true) { return hackCursor; } else { return defaultCursor; }
+			//float dist = Vector3.Distance(mouseRayHit.point, chr.transform.position);
+			// perhaps check the distance of the AI path instead of direct line-of-sight distance
+			// perhaps do a raycast to check for intervening obstacles?
+			//if (dist > chr.maxMoveWithAvailableAP) { valid = false; }
+			if (valid == true) { return moveCursor; } else { return defaultCursor; }
 		}
 		else return defaultCursor;
 	}
