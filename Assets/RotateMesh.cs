7b3905bb1b0fe12756e2bb18d1bda1bf246da6c2
Christer Kaitila <breakdance@mcfunkypants.com>
2019-07-23 08:29:39 -0700

    deathstar-style hologram planet fx v2

---
+++ Assets/RotateMesh.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMesh : MonoBehaviour
+{
+
+    public float xSpeed = 0;
+    public float ySpeed = 0;
+    public float zSpeed = 50;
+
+    void Update()
+    {
+        transform.Rotate (
+            xSpeed*Time.deltaTime,
+            ySpeed*Time.deltaTime,
+            zSpeed*Time.deltaTime);
+    }
+}
