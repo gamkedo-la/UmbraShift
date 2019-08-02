66918249c4bf7c87a278eaab602da46e1f33d1fe
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2019-08-02 12:59:34 -0400

    Chatbox Triggered by Investigation Text.  Player can investigate Bar Top to trigger a dialogue.

--- Assets/Scripts/Player Interaction/canBeInvestigated.cs
+++ Assets/Scripts/Player Interaction/canBeInvestigated.cs
@@ -7,19 +7,32 @@ public class canBeInvestigated : MonoBehaviour
 
     //Interacts with BaseCharacterClass to allow player to invetigate objects
 
-    public Chatbox chatbox;
+   [SerializeField]
+    private Chatbox chatbox;
+
+    [SerializeField]
+    private InteractionScreen passScreen;
+    [SerializeField]
+    private InteractionScreen failScreen;
 
     public int BasePercentageChanceToInvestigate = 95;
+
+    void Awake()
+    {
+        chatbox = FindObjectOfType<Chatbox>();
+    }
 
     public void Investigated()
     {
         Debug.Log("A clue!");
         //TODO: affect chatbox main text and/or options here.
+        chatbox.Open(passScreen);
     }
 
     public void FailedToFindAnything()
     {
         Debug.Log("Nothing of interest here...");
         //TODO: affect chatbox main text and/or options here.
+        chatbox.Open(failScreen);
     }
 }
