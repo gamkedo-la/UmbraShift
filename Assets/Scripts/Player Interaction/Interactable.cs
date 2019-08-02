80f0ed8d9d4a2acefdc586f45735fe73abb843dd
Christer Kaitila <breakdance@mcfunkypants.com>
2019-08-02 14:52:00 -0700

    Friendly NPC Chatbox Interactions (wip)

--- Assets/Scripts/Player Interaction/Interactable.cs
+++ Assets/Scripts/Player Interaction/Interactable.cs
@@ -7,11 +7,19 @@ public class Interactable : MonoBehaviour
     private Material defaultMat;
     private Renderer currentRenderer;
 
+    // Chatbox trigger code
+    private GameObject ChatboxGUI;
+    private bool alreadyChatted = false;
+    public InteractionScreen NPC_Chat;
+
+
     // Start is called before the first frame update
     void Start()
     {
         currentRenderer = GetComponent<Renderer>();
         defaultMat = currentRenderer.sharedMaterial;  // might need to do individual, check later
+        ChatboxGUI =  GameObject.Find("Chatbox"); // the GUI
+        alreadyChatted = false;
     }
 
     // Update is called once per frame
@@ -30,5 +38,14 @@ public class Interactable : MonoBehaviour
     {
 //        Debug.Log("Setting highlight material");
         currentRenderer.material = InputManager.instance.highlightedMat;
+
+        //Debug.Log("Testing NPC Chat Trigger...");
+        // FIXME: only trigger when not targetting in combat?
+        if (!alreadyChatted && ChatboxGUI != null && NPC_Chat != null) {
+            Debug.Log("Triggering an NPC Chat!");
+            Chatbox Chat = ChatboxGUI.GetComponent<Chatbox>();
+            Chat.Open(NPC_Chat);
+            alreadyChatted = true;
+        }
     }
 }
