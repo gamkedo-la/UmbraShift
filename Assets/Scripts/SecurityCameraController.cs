61300f667814a9337d8036c0ddb07c5e5e57e8d9
Carl <carl@qu-be.co.uk>
2019-08-13 20:18:37 +0100

    Test adding audio to Security Camera

---
+++ Assets/Scripts/SecurityCameraController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityCameraController : MonoBehaviour
+{
+
+    public void PlayCameraMovementAudio()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event:/Camera_Moving");
+    }
+}
