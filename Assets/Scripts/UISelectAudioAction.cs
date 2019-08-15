779aa2ae0e560c059e6f5d9c5da39dc3379bd165
Carl <carl@qu-be.co.uk>
2019-08-15 21:50:54 +0100

    added Select UI audio to character select buttons.

---
+++ Assets/Scripts/UISelectAudioAction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISelectAudioAction : MonoBehaviour
+{
+    public void PlayUISelectSound()
+    {
+        Debug.Log("Playing UI Select",this);
+        FMODUnity.RuntimeManager.PlayOneShot("event:/UI/UI_Select");
+    }
+}
