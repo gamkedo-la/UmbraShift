843653dc04c5f63fe878a7710e99c956e16b18a6
Carl <carl@qu-be.co.uk>
2019-08-18 19:40:53 +0100

    added player interaction 1

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -12,6 +12,8 @@ public class SoundManager : MonoBehaviour
     public string maleGruntMI = "event:/HumanMaleGruntMI";
     public string footSteps1 = "event:/HumanFootSteps_1";
     public string footSteps2 = "event:/HumanFootSteps_2";
+    public string footSteps4 = "event:/HumanFootSteps_4";
+    public string footSteps5 = "event:/HumanFootSteps_5";
     public string gunshotPistol1 = "event:/Gun_Shot_Pistol_1";
     public string gunshotPistol1_2shots = "event:/Gun_Shot_Pistol_1-burst2";
     public string gunshotPistol1_3shots = "event:/Gun_Shot_Pistol_1-burst3";
@@ -20,6 +22,7 @@ public class SoundManager : MonoBehaviour
     public string enemyAlerted2 = "event:/Alerted_Guard_2";
     public string enemyAlerted3 = "event:/Alerted_Guard_3";
     public string securityAlarm = "event:/Security Alarm";
+    public string playerObjectInteraction1 = "event:/Player_Interaction_1";
 
 
     public string inGameMusic = "event:/InGameMusic";
