8d8cfa941f568e843d4b00303a33ce3064c683bf
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2019-08-21 13:12:46 -0400

    Changing Stat Button to Toggles.  WIP.

---
+++ Assets/Scripts/Characters/CharacterGenerationUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterGenerationUI : MonoBehaviour
+{
+    public Toggle statToggle;
+
+    private Image toggleImage;
+
+    public Color selectedStatColor = Color.red;
+
+    void Awake()
+    {
+        toggleImage = GetComponent<Image>();
+    }
+
+    public void ChangeColor()
+    {
+        if (statToggle.isOn)
+        {
+            toggleImage.color = selectedStatColor;
+        }
+    }
+
+    //Check if any button in array is clicked
+
+
+    //Loop through button array and deselect each button
+
+    //Highlight the selected button. 
+
+    
+}
