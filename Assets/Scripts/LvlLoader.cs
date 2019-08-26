9b077ef1c84a659e262003c76a0c1ec4d34b34a9
ContrerasJ <32560547+ContrerasJ@users.noreply.github.com>
2019-08-25 23:34:25 -0400

    Named the buttons so that are not listed as 1,2,3. Added a level loading gameobject that holds the level loading script.

---
+++ Assets/Scripts/LvlLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LvlLoader : MonoBehaviour
+{
+
+    int currentSceneIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex == 0) {
+            LoadNextScene();
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        //Identifying the current scene and moving on to the scene that directly follows
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
