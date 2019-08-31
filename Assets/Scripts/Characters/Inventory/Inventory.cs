d0f21c0578f5b47be61c7dec04159e923ac5d902
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2019-08-31 11:33:04 -0400

    Inventory System.  Created base scripts for Inventory System. WIP

---
+++ Assets/Scripts/Characters/Inventory/Inventory.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    public int spaces = 8;
+
+    public List<Item> items = new List<Item>();
+    public List<int> itemsInSlot = new List<int>();
+
+    public bool Add(Item item)
+    {
+        if(item != item.isDefaultItem)
+        {
+            if(items.Count >= spaces)
+            {
+                Debug.Log("Inventory full");
+                return false;
+            }
+
+            if(item.stackable == false)
+            {
+                items.Add(item);
+                itemsInSlot.Add(1);
+            }
+            if(HasItem(item) == false && item.stackable == true)
+            {
+                items.Add(item);
+                itemsInSlot.Add(0);
+            }
+
+            if (onItemChangedCallback != null)
+            {
+                onItemChangedCallback.Invoke();
+            }
+            
+        }
+       return true;
+    }
+
+    public void AddToStack(Item item, int amount)
+    {
+        int index = items.IndexOf(item);
+        Debug.Log(item.name + " is in slot " + index);
+
+        if((itemsInSlot[index] + amount) >= item.maxItemsInSlot)
+        {
+            itemsInSlot[index] = item.maxItemsInSlot;
+        }
+        else
+        {
+            itemsInSlot[index] += amount;
+        }
+        Debug.Log("There are " + itemsInSlot[index].ToString() + " in slot " + index);
+        onItemChangedCallback.Invoke();
+    }
+
+    private bool HasItem(Item item)
+    {
+        return items.Exists(i => i == item);
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+        if(onItemChangedCallback != null)
+        {
+            onItemChangedCallback.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Characters/Inventory/Item.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    new public string name = "New Item";
+    public Sprite icon = null;
+    public bool isDefaultItem = false;
+
+    public bool stackable = false;
+
+    public int maxItemsInSlot = 0;
+
+    public string itemDescription = "Item description here.";
+
+    public virtual void Use()
+    {
+        Debug.Log("Using" + name);
+    }    
+
+}
