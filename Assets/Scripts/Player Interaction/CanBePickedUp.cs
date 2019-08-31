41e64f3ecb23658adb56f109588333793cd055dd
Brian Nielsen <47045697+brnielsen@users.noreply.github.com>
2019-08-31 11:55:01 -0400

    Inventory System.  Additional scripts WIP.

---
+++ Assets/Scripts/Player Interaction/CanBePickedUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanBePickedUp : MonoBehaviour
+{
+
+    public Item item;
+
+    private BaseCharacterClass baseCharacter;
+
+    private Inventory inventory;
+    // Start is called before the first frame update
+    void Awake()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            inventory.Add(item);
+        }
+    }
+}
