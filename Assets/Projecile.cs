9ac9108ba9015310511f5d179923538422aa912a
Anthony J <lordrahvin@gmail.com>
2019-09-11 00:50:42 -0700

    Playing around with creating flashy projectile and explosion effects.

---
+++ Assets/Projecile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projecile : MonoBehaviour
+{
+	[SerializeField] float speed = 5f;
+	[SerializeField] GameObject ExplosionPrefab;
+	float maxDistance = 50f;
+	float distance = 0f;
+
+    void Update()
+    {
+		distance = distance + (speed * Time.deltaTime);
+		Vector3 vec = transform.forward * (speed * Time.deltaTime);
+		transform.localPosition = transform.localPosition + vec;
+		if (distance >= maxDistance) { Destroy(this.gameObject); }
+    }
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		//TODO: work out collisions
+	}
+}
