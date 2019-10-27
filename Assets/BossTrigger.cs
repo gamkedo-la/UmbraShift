b8f7627fe4c3061108508f60d0cf78e77b4465c5
lordrahvin <lordrahvin@gmail.com>
2019-10-26 18:46:19 -0700

    Moved Boss to guard security door with machine gun. Made Boss larger and gave him a spotlight.  Added area trigger to start Boss' bluffing when approaching Boss area.  Boss does not move.

---
+++ Assets/BossTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossTrigger : MonoBehaviour
+{
+	[SerializeField] private InteractionScreen interaction;
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag=="Player")
+		{
+			PlayerCharacterData playerCharacterData = FindObjectOfType<PlayerCharacterData>();
+			if (playerCharacterData) { playerCharacterData.AdvanceStory(); }
+			Chatbox chatBox = FindObjectOfType<Chatbox>();
+			if (chatBox) { chatBox.Open(interaction); }
+			Destroy(this.gameObject);
+		}
+	}
+}
